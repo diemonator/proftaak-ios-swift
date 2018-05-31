af4849ec3415f3afe29308c4336e9f9fa8468538
Emil <e.karamihov@student.fontys.nl>
2018-05-31 09:50:22 +0200

    Empty Tabbed Project

---
+++ Proftaak_Oce/Proftaak_Oce/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Proftaak_Oce
+//
+//  Created by Emil Karamihov on 31/05/2018.
+//  Copyright © 2018 Fontys University. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
